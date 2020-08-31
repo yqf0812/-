fce0d79a9a2d933305d9cdd7b2f10026d0427129
yqf0812 <fessi@qq.com>
2020-08-31 17:11:51 +0800

    记账

--- app.js
+++ app.js
@@ -26,7 +26,19 @@ app.get('/categories', function (req, res) {
     let sliceArr = deepCopy(result);
     let timeArr = [];
     let total = 0;
+    let income = 0; //当前月收入金额
+    let expenditure = 0; //当前月支出金额
+    let allIncome = 0; //全年收入
+    let allExpenditure = 0; //全年支出
     let offset = parseInt(req.query.offset) * parseInt(req.query.limit);
+    //统计全年支出与收入的金额
+    for (let j = 0; j < sliceArr.length; j++) {
+        if (sliceArr[j].type === '1') {
+            allIncome = allIncome + parseInt(sliceArr[j].amount);
+        } else {
+            allExpenditure = allExpenditure + parseInt(sliceArr[j].amount);
+        }
+    }
     console.log(req.query)
     //时间过滤
     if (req.query.month === '') {
@@ -39,16 +51,26 @@ app.get('/categories', function (req, res) {
             }
         }
         total = timeArr.length;
-        // offset = 0 *  parseInt(req.query.limit);
     }
-    // console.log(timeArr)
+    //统计支出与收入的金额
+    for (let j = 0; j < timeArr.length; j++) {
+        if (timeArr[j].type === '1') {
+            income = income + parseInt(timeArr[j].amount);
+        } else {
+            expenditure = expenditure + parseInt(timeArr[j].amount);
+        }
+    }
     //分页
     timeArr = timeArr.splice((offset), parseInt(req.query.limit));
     res.json({
         code: 0,
         msg: 'ok',
         data: timeArr,
-        total: total
+        total: total,
+        income: income,
+        expenditure: expenditure,
+        allIncome: allIncome,
+        allExpenditure: allExpenditure
     })
 });
 
